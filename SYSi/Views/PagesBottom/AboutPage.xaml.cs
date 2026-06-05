58c995af357962a9af26a2234c5d2f55999ed1f3
Huynh Cong Xuan Mai <59072697+maihcx@users.noreply.github.com>
2026-06-05 21:42:25 +0700

    Add About page and localization
    Introduce an About page with view, viewmodel, and code-behind (AboutPage.xaml / .xaml.cs / AboutViewModel). The About view shows app identity, version (bound to ViewModel.AppVersion), framework/UI lib/license info, and an informational InfoBar about open-source libraries. Add localization keys for English, Vietnamese and default resources (app_description_summary, framework_title, ui_lib_title, license_title, page_about_title, page_about_summary, open_source_summary). Also tweak MainWindow navigation view to use a FadeInWithSlideAuto transition.

---
+++ SYSi/Views/PagesBottom/AboutPage.xaml.cs
@@ -0,0 +1,16 @@
+﻿namespace SYSi.Views.PagesBottom
+{
+    [PageMeta("page_about_title", "page_about_summary", SymbolRegular.Info24, 1000)]
+    public partial class AboutPage : INavigableView<AboutViewModel>
+    {
+        public AboutViewModel ViewModel { get; }
+
+        public AboutPage(AboutViewModel viewModel)
+        {
+            ViewModel = viewModel;
+            DataContext = this;
+
+            InitializeComponent();
+        }
+    }
+}
