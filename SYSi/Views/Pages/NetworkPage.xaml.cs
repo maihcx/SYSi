9d5522973038fb24236274e7d6d16e95be2e20a6
maihcx <59072697+maihcx@users.noreply.github.com>
2026-06-10 11:34:52 +0700

    Add Network page, viewmodel and localization
    Introduce a new Network feature: add NetworkViewModel, NetworkPage.xaml and NetworkPage.xaml.cs to display network adapters and their details (addresses, hardware info, connection state) bound to HardwareHostService.Networks. Add localization keys for network UI strings in en, vi and default resx files. Also bump OSPage PageMeta order index (6 -> 7) to accommodate the new page.

---
+++ SYSi/Views/Pages/NetworkPage.xaml.cs
@@ -0,0 +1,16 @@
+﻿namespace SYSi.Views.Pages
+{
+    [PageMeta("page_network_title", "page_network_summary", SymbolRegular.NetworkCheck24, 6, true)]
+    public partial class NetworkPage : INavigableView<NetworkViewModel>
+    {
+        public NetworkViewModel ViewModel { get; }
+
+        public NetworkPage(NetworkViewModel viewModel)
+        {
+            ViewModel = viewModel;
+            DataContext = this;
+
+            InitializeComponent();
+        }
+    }
+}
--- SYSi/Views/Pages/OSPage.xaml.cs
+++ SYSi/Views/Pages/OSPage.xaml.cs
@@ -1,6 +1,6 @@
 ﻿namespace SYSi.Views.Pages
 {
-    [PageMeta("page_os_title", "page_os_summary", SymbolRegular.DesktopPulse24, 6, true)]
+    [PageMeta("page_os_title", "page_os_summary", SymbolRegular.DesktopPulse24, 7, true)]
     public partial class OSPage : INavigableView<OSViewModel>
     {
         public OSViewModel ViewModel { get; }
